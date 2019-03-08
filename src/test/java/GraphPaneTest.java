301127afa81fec574033d5e81dfe835a7fd896aa
Pedro Victori <fosforilasa@gmail.com>
2019-03-08 18:01:38 +0000

    testing graph view

---
+++ src/test/java/GraphPaneTest.java
@@ -0,0 +1,24 @@
+import core.CircleGraph;
+import graph.GeneGraph;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GraphPaneTest extends Application {
+
+        @Override
+        public void start(Stage primaryStage) {
+            GeneGraph graph = new GeneGraph(1);
+            CircleGraph circleGraph = new CircleGraph(graph.getGraph(), 400.);
+            Pane pane = new StackPane(circleGraph);
+            Scene scene = new Scene(pane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
