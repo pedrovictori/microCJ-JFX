4c130c010d9bdbe16ce2531e13add3a3c166a5aa
Pedro Victori <fosforilasa@gmail.com>
2019-03-21 17:46:34 +0000

    testing

--- src/test/java/GraphPaneTest.java
+++ src/test/java/GraphPaneTest.java
@@ -1,26 +1,37 @@
-import core.Node;
-import graph.GeneLink;
-import graphs.CircleGraph;
 import graph.GeneGraph;
+import graph_diagram.GeneDiagram;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+
 public class GraphPaneTest extends Application {
 
-        @Override
-        public void start(Stage primaryStage) {
-            GeneGraph graph = new GeneGraph(1);
-            CircleGraph circleGraph = new CircleGraph<>(graph.getGraph(), 400.);
-            Pane pane = new StackPane(circleGraph);
-            Scene scene = new Scene(pane);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
+	@Override
+	public void start(Stage primaryStage) {
+		GeneGraph graph = GeneGraph.RandomlyActivatedGraph(1).turnNode("Oxygen_supply", true);
+		GeneDiagram circleGraph = new GeneDiagram(graph, 400.);
+		Pane pane = new StackPane(circleGraph);
+		Scene scene = new Scene(pane);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
-        public static void main(String[] args) {
-            launch(args);
-        }
-    }
+		Runnable helloRunnable = () -> {
+			graph.update();
+			circleGraph.updateActivationStatus();
+		};
+
+		ScheduledExecutorService executor = newScheduledThreadPool(1);
+		executor.scheduleAtFixedRate(helloRunnable, 0, 300, TimeUnit.MILLISECONDS);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/test/java/PlacementTest.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2013, 2014 Oracle and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  - Neither the name of Oracle nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import core.Tumor;
+import geom.Point3D;
+import gui.Xform;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author cmcastil
+ */
+public class PlacementTest extends Application {
+
+    final Group cellsRoot = new Group();
+    final Xform axisGroup = new Xform();
+    final Xform world = new Xform();
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
+    final Xform cameraXform = new Xform();
+    final Xform cameraXform2 = new Xform();
+    final Xform cameraXform3 = new Xform();
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+    private static final double AXIS_LENGTH = 250.0;
+    private static final double HYDROGEN_ANGLE = 104.5;
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 0.1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+
+    double mousePosX;
+    double mousePosY;
+    double mouseOldX;
+    double mouseOldY;
+    double mouseDeltaX;
+    double mouseDeltaY;
+
+    private int cells = 100;
+    private double cellRad = 10;
+    private List<Xform> cellShapes = new ArrayList<>();
+    private List<Point3D> cellCenters = new ArrayList<>();
+    private Pane graphPane;
+
+    private static final int INITIAL_TUMOR_SIZE = 100;
+    private Tumor tumor;
+
+    //   private void buildScene() {
+    //       cellsRoot.getChildren().add(world);
+    //   }
+    private void buildCamera() {
+        System.out.println("buildCamera()");
+        cellsRoot.getChildren().add(cameraXform);
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    }
+
+    private void buildAxes() {
+        System.out.println("buildAxes()");
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(false);
+        world.getChildren().addAll(axisGroup);
+    }
+
+    private void handleMouse(Scene scene, final Node root) {
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseOldX = me.getSceneX();
+                mouseOldY = me.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mouseOldX = mousePosX;
+                mouseOldY = mousePosY;
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseDeltaX = (mousePosX - mouseOldX);
+                mouseDeltaY = (mousePosY - mouseOldY);
+
+                double modifier = 1.0;
+
+                if (me.isControlDown()) {
+                    modifier = CONTROL_MULTIPLIER;
+                }
+                if (me.isShiftDown()) {
+                    modifier = SHIFT_MULTIPLIER;
+                }
+                if (me.isPrimaryButtonDown()) {
+                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);
+                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() + mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);
+                }
+                else if (me.isSecondaryButtonDown()) {
+                    double z = camera.getTranslateZ();
+                    double newZ = z + mouseDeltaX*MOUSE_SPEED*modifier;
+                    camera.setTranslateZ(newZ);
+                }
+                else if (me.isMiddleButtonDown()) {
+                    cameraXform2.t.setX(cameraXform2.t.getX() + mouseDeltaX*MOUSE_SPEED*modifier*TRACK_SPEED);
+                    cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY*MOUSE_SPEED*modifier*TRACK_SPEED);
+                }
+            }
+        });
+    }
+
+    private void handleKeyboard(Scene scene, final Node root) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case Z:
+                        cameraXform2.t.setX(0.0);
+                        cameraXform2.t.setY(0.0);
+                        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+                        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+                        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+                        break;
+                    case X:
+                        axisGroup.setVisible(!axisGroup.isVisible());
+                        break;
+                }
+            }
+        });
+    }
+
+    private void generateCells() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial whiteMaterial = new PhongMaterial();
+        whiteMaterial.setDiffuseColor(Color.WHITE);
+        whiteMaterial.setSpecularColor(Color.LIGHTBLUE);
+
+        final PhongMaterial greyMaterial = new PhongMaterial();
+        greyMaterial.setDiffuseColor(Color.DARKGREY);
+        greyMaterial.setSpecularColor(Color.GREY);
+
+        Random r = new Random();
+        for (int i = 0; i < 100; i++) {
+            Sphere sphere = new Sphere(1);
+            Point3D target = getRandomCoordinates(r);
+            Translate translation = new Translate(target.getX(), target.getY(), target.getZ());
+            sphere.getTransforms().clear();
+            sphere.getTransforms().addAll(translation);
+
+            sphere.setMaterial(greyMaterial);
+            Xform ncellx = new Xform();
+            ncellx.getChildren().add(sphere);
+            world.getChildren().add(ncellx);
+            cellShapes.add(ncellx);
+        }
+    }
+
+    private Point3D getRandomCoordinates(Random r) {
+        double radius = 10;
+        double[] rndArray = new double[3];
+        double rndSqrSum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            rndArray[i] = r.nextGaussian();
+            rndSqrSum += rndArray[i] * rndArray[i];
+        }
+
+        double factor = radius / Math.sqrt(rndSqrSum);
+
+        //The random Coordinates then are:
+        double x = rndArray[0]*factor;
+        double y = rndArray[1]*factor;
+        double z = rndArray[2]*factor;
+        return new Point3D(x, y, z);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        cellsRoot.getChildren().add(world);
+        cellsRoot.setDepthTest(DepthTest.ENABLE);
+
+        // buildScene();
+        buildCamera();
+        buildAxes();
+        generateCells();
+
+        Scene mainScene = new Scene(cellsRoot, 2048, 1024, true);
+        mainScene.setFill(Color.CORNSILK);
+
+        handleKeyboard(mainScene, world);
+        handleMouse(mainScene, world);
+
+        primaryStage.setTitle("microC2");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
+        mainScene.setCamera(camera);
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
